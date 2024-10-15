2aaf17834ad8fe323908eb79157015ed8a693e4c
Upendra Kumar <toxic74412@gmail.com>
2024-10-15 15:18:21 +0530

    added env at frontend

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,1 @@
-// export const BASE_URL = "http://localhost:8000/api/v1";
-// export const BASE_URL = "https://recallmoments.netlify.app/";
-export const BASE_URL = "https://recallmomentsapi.onrender.com/api/v1";
+export const BASE_URL = import.meta.env.VITE_BASE_API_URL;
