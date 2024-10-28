3a46a88900e14b5246eafb9e52dd310077be5e01
Upendra Kumar <toxic74412@gmail.com>
2024-10-28 12:24:54 +0530

    fix texts

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -94,7 +94,7 @@ export default function Header() {
               handleClick={closeUserMenu}
             />
             <NavItem
-              title="Login"
+              title="Get Started"
               hrefValue="/login"
               handleClick={closeUserMenu}
             />
@@ -114,4 +114,3 @@ export default function Header() {
     </header>
   );
 }
-
