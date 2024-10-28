f8ab652b518edfe08c15326fbb25c5daedbced51
Upendra Kumar <toxic74412@gmail.com>
2024-10-28 12:28:55 +0530

    fix spacing

--- frontend/src/components/dashboard/Banner.tsx
+++ frontend/src/components/dashboard/Banner.tsx
@@ -2,7 +2,7 @@ type Props = { username: string };
 
 export default function Banner({ username }: Props) {
   return (
-    <div className="h-48 sm:h-28 p-4 rounded-xl flex justify-center items-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] mb-6 bg-gradient-to-r from-green-800 via-green-600 to-green-800">
+    <div className="h-48 sm:h-28 p-4 rounded-xl mt-5 flex justify-center items-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] mb-6 bg-gradient-to-r from-green-800 via-green-600 to-green-800">
       <h1 className="text-6xl lg:text-4xl sm:text-3xl xs:text-2xl text-white font-display">
         Welcome back, {username}
       </h1>
