3dfb9237016af5e30003db8beca55a25a3b45f8d
Upendra Kumar <toxic74412@gmail.com>
2024-11-09 13:30:47 +0530

    fixing

--- frontend/src/utils/helper/supabaseClient.ts
+++ frontend/src/utils/helper/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import { supabaseProjectUrl, supabaseApiKey } from "../constants";
+console.log({ supabaseProjectUrl, supabaseApiKey });
 
 export const supabase = createClient(supabaseProjectUrl, supabaseApiKey);
