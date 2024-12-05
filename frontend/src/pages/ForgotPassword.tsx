262a7afd3ced114bd954d46eb337519976bc2987
Upendra Kumar <toxic74412@gmail.com>
2024-12-05 13:33:06 +0530

    reset forgot, reset component

--- frontend/src/pages/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword.tsx
@@ -8,14 +8,15 @@ export default function ForgotPassword() {
   return (
     <section className="base-paddings">
       <main className="pt-24">
-        <div className="min-h-[80vh] w-[70%] xl:w-full mx-auto py-10 px-10 md:px-6 flex items-center shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] bg-green-200/5">
+        <div className="min-h-[80vh] w-[70%] xl:w-full mx-auto py-10 px-10 md:px-6 flex items-center big-shadow bg-green-200/5">
           <div className="flex w-full gap-x-14 sm:flex-col-reverse sm:gap-y-10 sm:justify-center">
             <div className="mr-auto w-[100%] text-center">
               <h1 className="text-3xl font-display mb-1 text-white">
                 Forgot Password
               </h1>
               <p className="text-sm mb-6">
-                Enter your e-mail address and we'll give you reset instruction.
+                Enter your e-mail address and we'll give you password reset
+                instruction.
               </p>
               <ForgotPasswordForm />
               <div className="mt-2.5">
--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -8,14 +8,15 @@ export default function ResetPassword() {
   return (
     <section className="base-paddings">
       <main className="pt-24">
-        <div className="min-h-[80vh] w-[70%] xl:w-full mx-auto py-10 px-10 md:px-6 flex items-center shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] bg-green-200/5">
-          <div className="flex w-full gap-x-14 sm:flex-col-reverse sm:gap-y-14 sm:justify-center">
-            <div className="mr-auto w-[100%] text-center">
+        <div className="min-h-[80vh] w-[70%] xl:w-full mx-auto py-10 px-10 md:px-6 flex items-center big-shadow bg-green-200/5">
+          <div className="flex w-full gap-x-14 sm:flex-col-reverse sm:gap-y-10 sm:justify-center">
+            <div className="mr-auto w-[100%] text-center mt-5">
               <h1 className="text-3xl font-display mb-1 text-white">
                 Reset Password
               </h1>
               <p className="text-sm mb-6">
-                Enter a strong password to change your password.
+                Enter a password mixed with letters, symbols and numbers to
+                change your password.
               </p>
               <ResetPasswordForm />
               <div className="mt-2.5">
