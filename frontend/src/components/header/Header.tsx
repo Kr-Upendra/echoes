bce91c28321ee9db17c9f932e65a57570d932764
Upendra Kumar <toxic74412@gmail.com>
2025-02-20 20:54:29 +0530

    update header design

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,7 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { IoBookOutline, IoMenu } from "react-icons/io5";
-import { FaXmark } from "react-icons/fa6";
-import { useState } from "react";
+import { IoBookOutline } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import NavItem from "../common/NavItem";
 import { RootState } from "../../state";
@@ -14,15 +12,13 @@ import { FiEdit } from "react-icons/fi";
 
 import { IconType } from "react-icons";
 import { BiGridAlt } from "react-icons/bi";
-import useScreenWidth from "../../hooks/useScreenWidth";
 import { useMutation } from "@tanstack/react-query";
 import { logoutUser } from "../../api";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export default function Header() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const width = useScreenWidth();
-  const [showNavbar, setShowNavbar] = useState<boolean>(true);
   const currentUser = useSelector(
     (state: RootState) => state.currentUser.currentUserInfo
   );
@@ -49,10 +45,6 @@ export default function Header() {
     mutation.mutate();
   }
 
-  function closeUserMenu() {
-    setShowNavbar(true);
-  }
-
   return (
     <>
       <header className="flex items-center base-paddings backdrop-blur-[2px] pt-3 pb-6 fixed top-0 left-0 w-full z-50 bg-gradient-to-b from-green-500/20 to-[#0000000c]">
@@ -62,130 +54,44 @@ export default function Header() {
         ) : (
           <>
             <div className="md:hidden">
-              <NavItem
-                title="Dashboard"
-                hrefValue="/dashboard"
-                handleClick={closeUserMenu}
-              />
-              <NavItem
-                title="Memories"
-                hrefValue="/memories"
-                handleClick={closeUserMenu}
-              />
-              <NavItem
-                title="Journals"
-                hrefValue="/journals"
-                handleClick={closeUserMenu}
-              />
-              <NavItem
-                title="Profile"
-                hrefValue="/profile"
-                handleClick={closeUserMenu}
-              />
-              <Button
-                title="LOGOUT"
-                onclick={currentUser && handleClick}
-                extraStyle="border-red-700 ml-2 text-white bg-red-700 hover:bg-red-800"
-              />
+              <NavItem title="Dashboard" hrefValue="/dashboard" />
+              <NavItem title="Memories" hrefValue="/memories" />
+              <NavItem title="Journals" hrefValue="/journals" />
+              <NavItem title="Profile" hrefValue="/profile" />
+              <NavItem title="Settings" hrefValue="/settings" />
             </div>
-            <button
-              type="button"
-              className="hidden md:block cursor-pointer z-50"
-              onClick={() => setShowNavbar((prev) => !prev)}
+            <NavLink
+              to="/settings"
+              className={({ isActive }) =>
+                `hidden md:block ${
+                  isActive
+                    ? "text-green-500 font-display font-extrabold"
+                    : "text-white font-display font-extrabold"
+                }`
+              }
             >
-              {showNavbar ? (
-                <IoMenu className="text-2xl text-white" />
-              ) : (
-                <FaXmark className="text-2xl text-white" />
-              )}
-            </button>
+              <IoSettingsOutline className="w-6 h-6" />
+            </NavLink>
           </>
-        )}
-
-        {currentUser && !showNavbar && (
-          <div className="hidden md:block md:absolute md:z-30 md:top-0 md:left-0 md:w-full md:p-4 md:bg-black/95 md:backdrop-blur-sm md:rounded-md md:shadow-lg md:shadow-green-500/10 md:h-screen subsm:h-[40vh]">
-            <HeaderLogo />
-            <div className="md:flex md:flex-col md:justify-center md:items-center md:gap-y-3.5 h-[80%] mt-8 subsm:mt-0">
-              {width <= 520 ? (
-                <div className="subsm:hidden">
-                  <NavItem
-                    title="Dashboard"
-                    hrefValue="/dashboard"
-                    handleClick={closeUserMenu}
-                  />
-                  <NavItem
-                    title="Journals"
-                    hrefValue="/journals"
-                    handleClick={closeUserMenu}
-                  />
-                  <NavItem
-                    title="Memories"
-                    hrefValue="/memories"
-                    handleClick={closeUserMenu}
-                  />
-                  <NavItem
-                    title="Profile"
-                    hrefValue="/profile"
-                    handleClick={closeUserMenu}
-                  />
-                </div>
-              ) : (
-                <>
-                  <NavItem
-                    title="Dashboard"
-                    hrefValue="/dashboard"
-                    handleClick={closeUserMenu}
-                  />
-                  <NavItem
-                    title="Journals"
-                    hrefValue="/journals"
-                    handleClick={closeUserMenu}
-                  />
-                  <NavItem
-                    title="Memories"
-                    hrefValue="/memories"
-                    handleClick={closeUserMenu}
-                  />
-                  <NavItem
-                    title="Profile"
-                    hrefValue="/profile"
-                    handleClick={closeUserMenu}
-                  />
-                </>
-              )}
-
-              <Button
-                title="LOGOUT"
-                onclick={currentUser && handleClick}
-                extraStyle="border-red-700 ml-2 text-white bg-red-700 hover:bg-red-800"
-              />
-            </div>
-          </div>
         )}
       </header>
       {currentUser && (
-        <div className="hidden subsm:block subsm:fixed subsm:z-30 subsm:bottom-0 subsm:left-0 subsm:w-full subsm:bg-black/95 subsm:backdrop-blur-sm subsm:shadow-lg subsm:shadow-green-500/10">
-          <div className="flex items-center justify-center">
-            <IconLink
-              handleClick={closeUserMenu}
-              icon={BiGridAlt}
-              href="/dashboard"
-            />
-            <IconLink
-              handleClick={closeUserMenu}
-              icon={FiEdit}
-              href="/memories"
-            />
-            <IconLink
-              handleClick={closeUserMenu}
-              icon={IoBookOutline}
-              href="/journals"
-            />
-            <IconLink
-              handleClick={closeUserMenu}
-              icon={AiOutlineUser}
-              href="/profile"
-            />
+        <div className="hidden md:block md:fixed md:z-30 md:bottom-10 md:left-0 md:w-full">
+          <div className="border border-green-500/20 mx-auto rounded-full w-[80%] bg-black/95 backdrop-blur-sm shadow-lg shadow-green-500/10">
+            <div className="flex items-center justify-center">
+              <IconLink
+                icon={BiGridAlt}
+                href="/dashboard"
+                extraStyle="rounded-l-full"
+              />
+              <IconLink icon={FiEdit} href="/memories" />
+              <IconLink icon={IoBookOutline} href="/journals" />
+              <IconLink
+                icon={AiOutlineUser}
+                href="/profile"
+                extraStyle="rounded-r-full"
+              />
+            </div>
           </div>
         </div>
       )}
@@ -197,15 +103,16 @@ type Props = {
   icon: IconType;
   href: string;
   handleClick?: () => void;
+  extraStyle?: string;
 };
 
-export function IconLink({ icon: Icon, href, handleClick }: Props) {
+export function IconLink({ icon: Icon, href, handleClick, extraStyle }: Props) {
   return (
     <NavLink
       onClick={handleClick}
       to={href}
       className={({ isActive }) =>
-        `w-full py-4 flex justify-center font-bold items-start bg-green-200/5 border-x border-green-200/5 ${
+        `w-full py-4 flex justify-center font-bold items-start bg-green-200/5 border-x border-green-200/5 ${extraStyle} ${
           isActive
             ? "text-green-500 font-display font-extrabold"
             : "text-white font-display font-extrabold"
